f1f01368a9ad50b3afd38520d34a92c29f325da5
Jesse Jafa <jesse.jafa@gmail.com>
2019-11-02 10:46:35 +0200

    properly serving files & correct PORT n

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const path = require('path')
 
 
 const app = express();
 require('dotenv').config()
 const port = 5000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.static(path.join(__dirname, '../client/public')))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/public', 'index.html'))
+})
 require('./routes/apiRoutes')(app);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
