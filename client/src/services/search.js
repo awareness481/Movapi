faf5b9d262b5e9b3976abc842b4e7810dca16082
Jesse Jafa <jesse.jafa@gmail.com>
2019-11-13 07:46:33 +0200

    client implementantion to use sample data

--- client/src/services/search.js
+++ client/src/services/search.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
 const search = async (query) => {
-  let response = await axios.get('/api/s/' + query)
-  return response.data.Search;
+  // let response = await axios.get('/api/s/' + query)
+  // return response.data.results;
+
+  let response = await axios.get('/dev');
+  return response.data.results;
   
 };
 
