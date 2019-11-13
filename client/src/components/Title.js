35b3e08614241a718b4210d3db7412f14f7f32ca
Jesse Jafa <jesse.jafa@gmail.com>
2019-11-13 07:44:32 +0200

    Initial component for each search result

---
+++ client/src/components/Title.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const Title = ({result}) => {
+  const { 
+    title,
+    overview,
+    poster_path,
+    release_date,
+    genre_ids,
+    id,
+    original_title,
+    original_language,
+    backdrop_path,
+    popularity,
+    vote_count,
+    video,
+    vote_average,
+
+  } = result;
+
+  return (
+    <div className='title' key={id}>
+      {/* <div className='title__poster'>
+        <img className='title__poster--image' src={Poster} />
+      </div> */}
+      <div className='title--hero'>
+        <div className='title__poster'>
+          <img src={`https://image.tmdb.org/t/p/w500${poster_path}`} className='title__poster--image'/>
+        </div>
+        <div className='title--header'>
+          <h2 className='title--header__title'>{title}</h2>
+          <span className='title--header__year'>{release_date}</span>
+        </div>
+        <p className='title__plot'>{overview}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Title;
