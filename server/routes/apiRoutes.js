962f1004bb72a1b48a25a0473145c29df2d4dbd5
Jesse Jafa <jesse.jafa@gmail.com>
2019-11-13 07:45:00 +0200

    modified api to use sample data instead of real api

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -1,7 +1,10 @@
 const axios = require('axios');
+const sample = require('./sample.js');
 
 const key = process.env.API_KEY;
 const baseURL = 'http://www.omdbapi.com/';
+
+
 
 module.exports = (app) => {
   app.get('/api/id/:id',  async (req, res) => {
@@ -14,13 +17,15 @@ module.exports = (app) => {
 
   app.get('/api/s/:query', async (req, res) => {
     const results = await axios.get(
-      `${baseURL}?s=${req.params.query}&apikey=${key}`
+      `${baseURL}?s=${req.params.query}&page=2&apikey=${key}`
       )
       .catch(e => console.log(e));
       res.send(results.data)
   });
 
   app.get('/api/results/:query/:type?/:y?/:page?', (req, res) => {
-    res.send(req.params);
+    res.send(sample);
   })
+
+  app.get('/dev', (req, res) => res.send(sample))
 }
