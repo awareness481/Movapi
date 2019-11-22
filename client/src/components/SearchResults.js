87c48ee0620f9138f17b0d55bb3f073ae4a956cb
Jesse Jafa <jesse.jafa@gmail.com>
2019-11-22 09:23:48 +0200

    Implemented antd List and Pagination components for search results

--- client/src/components/SearchResults.js
+++ client/src/components/SearchResults.js
@@ -1,27 +1,29 @@
 import React, {useState, useEffect} from 'react';
 import Title from './Title';
+// import SearchPagination from './SearchPagination';
+import { List } from 'antd'
 import search  from '../services/search';
 
 const SearchResults = ({query}) => {
   const [data, setData] = useState([]);
+  const [limit, setLimit] = useState(0);
 
   useEffect(() => {
     const fetchData = async () => {
       const temp =  await search(query);
-      setData(temp)
+      setData(temp.results)
+      setLimit(temp.total_results)
     }
     fetchData()
   }, [])
 
   return (
     <div className='search-results'>
-      {
-        data && data.length > 0 ?
-           data.map((e) => {
-             return <Title result={e} />
-          })
-          : ''
-      }
+      {data ? <List
+      pagination={true}
+      dataSource={data}
+      renderItem={item => <List.Item><Title result={item} /></List.Item>}
+      /> : ''}
     </div>
   );
 }
