e0c142e3f5317cfee33ed66ba5907f5c1e654484
Jesse Jafa <jesse.jafa@gmail.com>
2019-11-25 09:25:26 +0200

    Initial styling and layout for the searchform

--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import SearchResults from './SearchResults';
 
+import { Input } from 'antd';
+
+
+
 const SearchForm = () => {
   const [query, setQuery] = useState('');
   const [resultsVisibility, setResultsVisibility] = useState(false);
@@ -10,18 +14,18 @@ const SearchForm = () => {
     setResultsVisibility(true);
   }
 
+  const { Search } = Input;
+
   return (
-    <div className="control">
-      <form onSubmit={handleSubmit}>
-        <input 
-          className="input"
+    <div className="search">
+      <form className='search__form' onSubmit={handleSubmit}>
+        <Search
+          className='search__form__input'
+          placeholder="input search text"
+          enterButton="Discover"
           value={query}
           onChange={e => setQuery(e.target.value)}
-          type="text"
-          placeholder="Normal input"
-          name='search'
         />
-        <input type='submit' value='submit' />
       </form>
       {resultsVisibility ? <SearchResults query={query}/> : ""}
     </div>
